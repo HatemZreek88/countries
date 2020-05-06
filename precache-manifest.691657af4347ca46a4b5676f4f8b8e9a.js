87418b653297fe33176044ce05b0edf4251def51
HatemZreek88 <hatem.zreek88@gmail.com>
2020-05-07 00:44:53 +0200

    Updates

---
+++ precache-manifest.691657af4347ca46a4b5676f4f8b8e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d929a994ba5b7de6b173defa2450611e",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "156c16b7a262ca03d564",
+    "url": "/countries/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e27cd74d5d9bd7f27c6b",
+    "url": "/countries/static/css/main.5ca43eac.chunk.css"
+  },
+  {
+    "revision": "156c16b7a262ca03d564",
+    "url": "/countries/static/js/2.7785854a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/countries/static/js/2.7785854a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27cd74d5d9bd7f27c6b",
+    "url": "/countries/static/js/main.7aefdb14.chunk.js"
+  },
+  {
+    "revision": "615f38357286920229da",
+    "url": "/countries/static/js/runtime-main.af34b7ac.js"
+  }
+]);
